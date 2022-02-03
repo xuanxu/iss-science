91acdb7f61361584accf7767005688546a774183
Juanjo Bazán <jjbazan@gmail.com>
2022-02-03 21:28:41 +0100

    lower name in db

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,7 +2,7 @@ class Developer < ApplicationRecord
   has_and_belongs_to_many :experiments
 
   default_scope { order(:name) }
-  scope :by_initial, ->(initial) { where("name LIKE ?", initial.downcase + "%") }
+  scope :by_initial, ->(initial) { where("lower(name) LIKE ?", initial.downcase + "%") }
 
   def self.initials
     Developer.unscoped.all.select(:name).map {|e| e.name[0].upcase}.uniq.compact.sort
