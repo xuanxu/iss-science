7db2fadebdc251a93c58460976948ff0564e8936
Juanjo Bazán <jjbazan@gmail.com>
2022-09-07 13:56:32 +0200

    Use env vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def admin_required
-    http_basic_authenticate_or_request_with name: "lolo", password: "lolo" #name: ENV['ISS_USER'].to_s, password: ENV['ISS_PASS'].to_s
+    http_basic_authenticate_or_request_with name: ENV['ISS_USER'].to_s, password: ENV['ISS_PASS'].to_s
   end
 end
