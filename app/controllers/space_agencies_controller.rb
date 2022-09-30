ee7f374db8a0c9ded112a842ee874ce85ec3e89a
Juanjo Bazán <jjbazan@gmail.com>
2022-09-30 12:16:13 +0200

    Cross space agencies and subcategories information

--- app/controllers/space_agencies_controller.rb
+++ app/controllers/space_agencies_controller.rb
@@ -5,5 +5,7 @@ class SpaceAgenciesController < ApplicationController
 
   def show
     @space_agency = SpaceAgency.find(params[:id])
+    @experiments = @space_agency.experiments
+    @experiments = @experiments.where(subcategory_id: params[:by_subcategory]) if params[:by_subcategory].present?
   end
 end
--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -5,5 +5,7 @@ class SubcategoriesController < ApplicationController
 
   def show
     @subcategory = Subcategory.find(params[:id])
+    @experiments = @subcategory.experiments
+    @experiments = @experiments.where(space_agency_id: params[:by_space_agency]) if params[:by_space_agency].present?
   end
 end
